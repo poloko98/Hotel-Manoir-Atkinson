e6bf63bf229bf467a4bfed8ff9a758c749c45512
Vasho <nicolasfernz@gmail.com>
2019-11-06 17:50:30 -0400

    mod contacto

--- atkn/urls.py
+++ atkn/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'^contacto/$', views.contacto, name='contacto'),
 ]
--- atkn/views.py
+++ atkn/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'atkn/index.html', {})
 
+def contacto(request):
+    return render(request, 'atkn/contacto.html', {})
